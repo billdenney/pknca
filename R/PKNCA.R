0b792604574dab1f34b7b948f6fd604e78f11baf
Bill Denney <wdenney@humanpredictions.com>
2018-06-12 22:46:08 -0400

    Remove plyr dependency

--- R/PKNCA.R
+++ R/PKNCA.R
@@ -30,7 +30,6 @@
 #' @importFrom lattice xyplot
 #' @importFrom nlme fixef getData gnls intervals lme nlme ranef
 #' @importFrom parallel mclapply
-#' @importFrom plyr rbind.fill
 #' @importFrom stats AIC as.formula coef confint formula glm logLik lm 
 #'   median model.frame na.exclude na.omit predict sd update 
 #'   update.formula
