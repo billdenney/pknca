bf0a3efecae7f7770d44701af7b6c5a24557f5dc
Devon Bartlett <dbartlett@humanpredictions.com>
2025-01-08 13:57:55 -0600

    adding testing coverage for sparse_mean() within `sparse.R`

--- tests/testthat/test-sparse.R
+++ tests/testthat/test-sparse.R
@@ -42,3 +42,22 @@ test_that("sparse_auc_df and sparse_auc_se are in the parameter list (#292)", {
     all(c("sparse_auc_df", "sparse_auc_se") %in% names(get.interval.cols()))
   )
 })
+
+test_that("sparse_mean", {
+  d_sparse <-
+    data.frame(
+      id = c(1L, 2L, 3L, 1L, 2L, 3L, 1L, 2L, 3L, 4L, 5L, 6L, 4L, 5L, 6L, 7L, 8L, 9L, 7L, 8L, 9L),
+      conc = c(0, 0, 0,  1.75, 2.2, 1.58, 4.63, 2.99, 1.52, 3.03, 1.98, 2.22, 3.34, 1.3, 1.22, 3.54, 2.84, 2.55, 0.3, 0.0421, 0.231),
+      time = c(0, 0, 0, 1, 1, 1, 6, 6, 6, 2, 2, 2, 10, 10, 10, 4, 4, 4, 24, 24, 24),
+      dose = c(100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100, 100)
+    )
+  
+  sparse_pk <- as_sparse_pk(d_sparse)
+  sparse_pk_wt <- sparse_auc_weight_linear(sparse_pk)
+  sparse_pk_mean <- sparse_mean(sparse_pk_wt, sparse_mean_method="arithmetic mean")
+  
+  expect_equal(
+    sparse_pk_mean[[7]]$mean_method,
+    "arithmetic mean"
+  )
+})
